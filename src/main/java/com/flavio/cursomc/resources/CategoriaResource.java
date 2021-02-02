e8f7b9b2fa67d948cf669b3b452d448a88b40375
flavioportugal20 <flavioportugal20@gmail.com>
2021-02-02 16:42:13 -0300

    Inserindo nova Categoria com POST

--- src/main/java/com/flavio/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/flavio/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,16 @@
 package com.flavio.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.flavio.cursomc.domain.Categoria;
 import com.flavio.cursomc.services.CategoriaService;
@@ -28,6 +29,18 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(obj);
 
 	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().
+				path("/{id}").buildAndExpand(obj.getId()).toUri();		
+		return ResponseEntity.created(uri).build();
+
+	}
+	
+	
 	
 	
 }
