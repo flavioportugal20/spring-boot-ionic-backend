aee51d37001bad2a1b0224a299cfb5e88311df78
flavioportugal20 <flavioportugal20@gmail.com>
2021-02-06 12:20:13 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/flavio/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/flavio/cursomc/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 import com.flavio.cursomc.domain.Cliente;
+import com.flavio.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
