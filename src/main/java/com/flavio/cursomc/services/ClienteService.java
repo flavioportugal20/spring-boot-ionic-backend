89f1dea39e7de73bebf66e47e44223120bac870a
flavioportugal20 <flavioportugal20@gmail.com>
2021-02-05 23:59:13 -0300

    Ajustando e testando a deleção de Cliente

--- src/main/java/com/flavio/cursomc/services/ClienteService.java
+++ src/main/java/com/flavio/cursomc/services/ClienteService.java
@@ -60,7 +60,7 @@ public class ClienteService{
 		try {
 			repo.deleteById(id);
 		}catch(DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir um cliente que possui produtos!");
+			throw new DataIntegrityException("Não é possível excluir um cliente que possui pedidos!");
 		}
 	}
 	
