80e96cdc04401b1cc834192940742d1d2f8d2cc7
flavioportugal20 <flavioportugal20@gmail.com>
2021-02-07 13:58:45 -0300

    Busca de produtos por nome  categorias

---
+++ src/main/java/com/flavio/cursomc/services/ProdutoService.java
@@ -0,0 +1,39 @@
+package com.flavio.cursomc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.flavio.cursomc.domain.Categoria;
+import com.flavio.cursomc.domain.Produto;
+import com.flavio.cursomc.domain.Produto;
+import com.flavio.cursomc.repositories.CategoriaRepository;
+import com.flavio.cursomc.repositories.ProdutoRepository;
+import com.flavio.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService{
+	
+	@Autowired
+	ProdutoRepository repo;
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
+	public Produto find(Integer id) {
+		Optional<Produto> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);	
+	}
+	
+}
